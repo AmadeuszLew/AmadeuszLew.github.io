e97976c66858de74cdb951e53bc2a49243af3be9
AmadueszLew <lewandowskiamadeusz99@gmail.com>
2022-11-03 00:35:22 +0100

    change hover to on click

--- src/app/projects/detail/detail.component.ts
+++ src/app/projects/detail/detail.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { Project } from '../project.model';
 import { ProjectDetail } from '../projectDetail.model';
 import { ProjectsService } from '../projects.service';
 @Component({
@@ -11,13 +10,16 @@ import { ProjectsService } from '../projects.service';
 })
 export class DetailComponent implements OnInit {
   project!:ProjectDetail;
-
+  viewFullSizePhoto: boolean[]=[];
   constructor(private route: ActivatedRoute,private projectService:ProjectsService) { }
 
   ngOnInit(): void {
     let id=this.route.snapshot.params['id'];
     this.project=this.projectService.getSingleProject(id)!
-    console.log(this.project)
+     // Creates the array with inactive state initially for all category items. 
+    this.viewFullSizePhoto = Array(this.project.features.length).fill(false);
   }
-
+  viewPhotoFunc(index:number) {
+    this.viewFullSizePhoto[index] = !this.viewFullSizePhoto[index];
+  }
 }
